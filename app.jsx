a76f3b099f0f4526bf323b9e03620a2aa6f12334
Paul Chin <pchinjr@gmail.com>
2016-12-28 16:50:59 -0500

    hello world

---
+++ app.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+// App.propTypes = {
+//   title: React.PropTypes.string,
+//   players: React.PropTypes.array,
+// };
+
+ReactDOM.render(<App />, document.getElementById('container'));
